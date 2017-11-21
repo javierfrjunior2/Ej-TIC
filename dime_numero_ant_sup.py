72a32fb0846b23ed79020c645017eea3655ff26c
javierfrjunior2 <33655350+javierfrjunior2@users.noreply.github.com>
2017-11-21 11:32:02 +0100

    Delete dime_numero_ant_sup.py

--- dime_numero_ant_sup.py
+++
@@ -1,7 +0,0 @@
-def dime_numero_ant_post():
-    numero=input("Dime un número")
-    for cont in range(numero-3,numero,1):
-        print cont,
-    for cont in range(numero+1,numero+4,1):
-        print cont,
-dime_numero_ant_post()        
