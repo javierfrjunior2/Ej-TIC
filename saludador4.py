1118e41788824b4093a4187c77f6d4a5507301d6
javierfrjunior2 <33655350+javierfrjunior2@users.noreply.github.com>
2017-11-21 11:34:36 +0100

    Delete saludador4.py

--- saludador4.py
+++
@@ -1,9 +0,0 @@
-def sumador():
-    x=input("Dime el primer número")
-    y=input("Dime el segundo número")
-    z=input("Dime el tercer número")
-    p=input("Dime el cuarto número")
-    u=input("Dime el quinto número")
-    print"la suma vale"
-    print x,"+",y,"+",z,"+",p,"+",u,"=",x+y+z+p+u
-sumador()
