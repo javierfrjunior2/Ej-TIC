026c19384b5df1cd762915dab821cadd3e938f7a
javierfrjunior2 <33655350+javierfrjunior2@users.noreply.github.com>
2017-11-21 11:34:30 +0100

    Delete pig_latin.py

--- pig_latin.py
+++
@@ -1,9 +0,0 @@
-def pig_latin():
-    texto=raw_input("Escribe un texto")
-    for cont in range(0,len(texto),1):
-        if texto[cont]=="A" or texto[cont]=="a" or texto[cont]=="E" or texto[cont]=="e" or texto[cont]=="I" or texto[cont]=="i" or texto[cont]=="O" or texto[cont]=="o" or texto[cont]=="U" or texto[cont]=="u":
-            print "u",
-        else:
-            print texto[cont],
-
-pig_latin()        
