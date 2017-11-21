e10052604a22d6e328af16a6c977d21b35c98e89
javierfrjunior2 <33655350+javierfrjunior2@users.noreply.github.com>
2017-11-21 11:32:09 +0100

    Delete divisor_decenas_unidades.py

--- divisor_decenas_unidades.py
+++
@@ -1,8 +0,0 @@
-def divisor_decenas_unidades():
-    numero=input("Dime un numero de dos cifras bombom de licor")
-    x=numero/10
-    y=numero%10
-    suma= x+y
-    print "El número es", suma
-divisor_decenas_unidades()    
-
