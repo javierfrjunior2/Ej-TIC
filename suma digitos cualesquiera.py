a829d905024a70e2c9c88de4fbd0c4440a77f477
javierfrjunior2 <33655350+javierfrjunior2@users.noreply.github.com>
2017-11-21 11:34:49 +0100

    Delete suma digitos cualesquiera.py

--- suma digitos cualesquiera.py
+++
@@ -1,9 +0,0 @@
-def suma_cifras():
-    suma=0
-    numero=input("Dime un numero cualquiera")
-    while numero>0:
-        suma=suma+numero%10
-        numero=numero/10
-    print suma+numero
-suma_cifras()
-    
