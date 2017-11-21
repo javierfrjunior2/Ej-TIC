a90a07ac8ba9b534f58b1d69d821a61275b8dd8c
javierfrjunior2 <33655350+javierfrjunior2@users.noreply.github.com>
2017-11-21 11:31:39 +0100

    Delete contador_aes.py

--- contador_aes.py
+++
@@ -1,8 +0,0 @@
-def contador_aes():
-    palabra=raw_input("Dime una palabra")
-    suma=0
-    for cont in range (0,len(palabra),1):
-        if palabra[cont]=='a':
-            suma=suma+1
-    print"En la palabra ",palabra," hay ",suma," aes"
-contador_aes()        
