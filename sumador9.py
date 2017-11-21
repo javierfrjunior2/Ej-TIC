9594e1516ca5020769dd9e1d8a6745f8704727db
javierfrjunior2 <33655350+javierfrjunior2@users.noreply.github.com>
2017-11-21 11:35:16 +0100

    Delete sumador9.py

--- sumador9.py
+++
@@ -1,9 +0,0 @@
-def sumador_acumulado():
-    n=input("Dime hasta que numero quieres sumar")
-    suma=0
-    for cont in range(1,n+1,1):
-        suma=suma+cont
-    print "suma=",suma
-sumador_acumulado()
-    
-    
