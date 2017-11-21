353766bfd69a8f4cc6e7f7c9f209bb334dd1e8fa
javierfrjunior2 <33655350+javierfrjunior2@users.noreply.github.com>
2017-11-21 11:35:35 +0100

    Delete texto_aleatorio.py

--- texto_aleatorio.py
+++
@@ -1,7 +0,0 @@
-def texto_aleatorio():
-    texto=raw_input("Dime cualquier cosa")
-    todo="a"or"e"or"i"or"o"or"u"    
-    for cont in range(0,len(texto),1):
-        if texto[cont]==todo:
-                print texto       
-texto_aleatorio()                   
